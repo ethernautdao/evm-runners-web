231d305f52de0c3a2ffb99ffc2d2a068ec4a70e7
kyrers <kyrers.eth@gmail.com>
2023-05-30 22:01:31 +0100

    Initial leaderboard page styling

--- components/leaderboards/Leaderboards.tsx
+++ components/leaderboards/Leaderboards.tsx
@@ -1,13 +1,44 @@
 import useLeaderboards from "@/hooks/useLeaderboards";
+import { Leadeboard } from "@/model/Leaderboard";
 import styles from "@/styles/Leaderboard.module.css";
 import { NextPage } from "next";
+import { useState } from "react";
 
 const Leaderboard: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLevel, setSelectedlevel] = useState<Leadeboard>();
   const { data, error, isLoading } = useLeaderboards();
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLevelClick = (item: Leadeboard) => {};
 
   return (
     <div className={styles.leaderboardLayout}>
-      <h1 data-cy="leaderboard-title">Leaderboard</h1>
+      <div className={styles.leaderboardDropdown} onClick={toggleDropdown}>
+        <span>
+          {selectedLevel ? selectedLevel.levelName : "Select level..."}
+          {isOpen ? (
+            <div className={styles.arrowUp} />
+          ) : (
+            <div className={styles.arrowDown} />
+          )}
+        </span>
+        {isOpen && (
+          <div className={styles.leaderboardDropdownMenu}>
+            {data?.map((level: Leadeboard, index: number) => {
+              return (
+                <p
+                  key={`level-${level.levelId}`}
+                  onClick={() => handleLevelClick(level)}
+                >{`${index + 1}. ${level.levelName}`}</p>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
