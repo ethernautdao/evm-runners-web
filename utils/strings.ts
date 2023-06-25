fcb355393bcea41a225fe545468ffccf860b26d9
kyrers <kyrers.eth@gmail.com>
2023-06-25 17:20:08 +0100

    Update the install command

--- utils/strings.ts
+++ utils/strings.ts
@@ -2,7 +2,7 @@ export const appTitle = "EVM Runners";
 export const gameDescription =
   "A terminal-based game for developers with EVM-based challenges";
 export const installCommand =
-  "curl -L https://raw.githubusercontent.com/ethernautdao/evm-runners-cli/main/install.sh | bash";
+  "curl -L https://api.evmr.sh/setup | bash";
 export const initialLoadText =
   "Loading. If you see this screen you most likely need to enable javascript for this website!";
 export const loadingText = "Loading";
